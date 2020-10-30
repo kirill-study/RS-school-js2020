189ca0c6b839e386f4b57b88cc46a9171329bbf1
Kirill Pogorelov <23706384+kirillpogo-study@users.noreply.github.com>
2020-10-30 16:38:31 +0300

    refactor: commented code, console.logs

--- momentum/main.js
+++ momentum/main.js
@@ -47,7 +47,6 @@ function updateDate() {
     let month = monthsList[now.getMonth()]
     let ndate = now.getDate()
     date.innerHTML = `${day}, ${ndate} ${month}`
-    //window.setTimeout(updateDate, 1000)
 }
 
 function zeroPad(n) {
@@ -77,27 +76,7 @@ function nextTime(timeOfDay) {
     return arrayTimes[arrayTimes.indexOf(timeOfDay)+1]
 }
 
-function nextImageNumber(timeOfDay, hour) {
-    //console.log('click gets into nextImageNumber', Math.round(Math.random()*5))
-    //should return not random of 6, but next one, or start at the beginning
-    //should go into next time of day
-
-    //if (i != 6) i++
-    //return zeroPad(imagesArray[i])
-    //return hour
-
-    return zeroPad(imagesArray[Math.round(Math.random()*5)])
-
-    if (timeOfDay == 'night') return imagesArray[hour]
-    if (timeOfDay == 'morning') return imagesArray[hour-6]
-    if (timeOfDay == 'afternoon') return imagesArray[hour-12]
-    if (timeOfDay == 'evening') return imagesArray[hour-18]
-
-    //return zeroPad(Math.round(Math.random()*19)+1)
-}
-
 let imagesArray
-let imagesArrayAll = []
 
 function makeImagesArray() {
     imagesArray = []
@@ -108,23 +87,12 @@ function makeImagesArray() {
         }
         randomN = Math.round(Math.random()*19 + 1)
     }
-    //imagesArray.push(...imagesArray)
     console.log(imagesArray)
 }
 
-let innerTimeOfDay
-
 function nextImage(timeOfDay, hour) {
-    //console.log('click gets into nextImage')
     const img = document.createElement('img')
-    //let innerNextImage = nextImageNumber(timeOfDay, hour)
-    //let innerTimeOfDay = timeOfDay
-    //if (innerNextImage == 6) {
-    //    innerTimeOfDay = nextTime(innerTimeOfDay)
-    //    innerNextImage = 0
-    //}
 
-    //console.log(hour, imagesArray[hour%6], 'strange')
     console.log(`assets/${timeOfDay}/${zeroPad(imagesArray[(hour-1)%6])}.jpg`)
     img.src = `assets/${timeOfDay}/${zeroPad(imagesArray[(hour-1)%6])}.jpg`
     img.onload = () => {
@@ -142,19 +110,12 @@ function recalcToD(hour) {
     else if (hour < 12) return 'morning'
     else if (hour < 18) return 'afternoon'
     else if (hour < 24) return 'evening'
-    //else if (hour >= 24) {
-     //   hour = 0
-      //  i = 0
-       // return 'night'
-    //}
-    // my mistake -- can't pass hour less than it is, see images before current time
 }
 
 let hourWrapper
+
 function setBgGreet(hour) {
 
-    console.log(hour, 'testing')
-    //console.log('click gets into setBgGreet', hour)
     if (hour.typeOf != 'number') {
         let now = new Date()
         hour = now.getHours()
@@ -162,6 +123,7 @@ function setBgGreet(hour) {
         i++
         hourWrapper = (hour+i > 24) ? hour+i - 24 : hour+i
     }
+
     if (hour < 6) {
         greeting.textContent = 'Good Night, '
         if (clicked) nextImage(recalcToD(hourWrapper),hourWrapper)
@@ -182,7 +144,6 @@ function setBgGreet(hour) {
     }
 
     else if (hour < 18) {
-        console.log(hour, imagesArray[hour%6], 'strange')
         greeting.textContent = 'Good Afternoon, '
         if (clicked) nextImage(recalcToD(hourWrapper),hourWrapper)
         else {
@@ -340,8 +301,6 @@ async function getWeather() {
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.textContent}&lang=en&appid=9d314bcec01fde2cf167a6bba6e9e512&units=metric`
   const res = await fetch(url)
   const data = await res.json()
-  //console.log(data.weatheweatherIcon.classList.add(`owf-${data.weather[0].id}`);
-    console.log(data.main)
   if (!data.main) {
       city.textContent = "Can't find such city"
       temp.textContent = ''
@@ -357,7 +316,6 @@ async function getWeather() {
   localStorage.setItem('wind', wind.innerText)
   icon.className = 'weather-icon owf';
   icon.classList.add(`owf-${data.weather[0].id}`)
-  //humidity.textContent = data.weather[0].description
 }
 
 
@@ -388,13 +346,10 @@ goal.addEventListener('blur', setGoal)
 
 nextButton.addEventListener('click', coolDownWrapperBg)
 
-//nextButton.addEventListener('click', console.log('works??'))
 updateTime()
 updateDate()
 getName()
 getGoal()
 getCity()
-//getQuote()
 setBgGreet(hoursGlobal)
-//getQuote()
 //#endregion event listeners and function calls
